58bf252ba1d4f3823870e6326827a6b6fef072a2
seventeenthey <sylvia_zhou1026@hotmail.com>
2019-03-02 16:41:24 -0500

    Clean and Build
    Modify folder

--- sampleWeb/js/slider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-$(".variable").slick({
-	/* initialization of lick */
-        dots: true,
-        infinite: true,
-        variableWidth: true,
-        centerMode: true
-      });
-})
